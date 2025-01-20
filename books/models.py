f1d0d71071c5c763eb6fc98f5511e6b068bc2432
sergiolttz <sergiolattanzio05@gmail.com>
2025-01-20 18:18:04 -0400

    modelo book creado

---
+++ books/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    avilable = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title  
+
