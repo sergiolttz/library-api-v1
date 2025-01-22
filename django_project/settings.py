5ad64ff4d9186c14692385880015a174701a2f36
sergiolttz <sergiolattanzio05@gmail.com>
2025-01-22 17:46:37 -0400

    endpoint list-books

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,7 +37,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    #local apps
     'books',
+    'apis',
+    #third party apps
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- django_project/urls.py
+++ django_project/urls.py
@@ -21,5 +21,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('books.urls')),
+    path('api/', include('apis.urls')),
 
 ]
